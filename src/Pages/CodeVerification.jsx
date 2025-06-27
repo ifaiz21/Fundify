f4325bcea35497b2b2d3fadef146c243d99347a5
ifaiz21 <mohammadfaiz1971@gmail.com>
2025-06-27 17:58:49 +0500

    path change in Fend

--- src/Pages/CodeVerification.jsx
+++ src/Pages/CodeVerification.jsx
@@ -83,7 +83,7 @@ const CodeVerificationPage = () => {
             console.log("Sending verification:", { email, code });
 
             // Using axios for API call
-            const response = await axios.post("https://server-fundify.up.railway.app//api/auth/code-verification", {
+            const response = await axios.post("https://server-fundify.up.railway.app/api/auth/code-verification", {
                 email,
                 code,
             });
